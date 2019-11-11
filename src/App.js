5b9bd1a1b2f99817ede4ae62a10375ad374054e6
Zelling <zelling@localhost.localdomain>
2019-11-11 14:45:30 -0300

    Components & UI Section Done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-function App() {
+import Searchbar from "./Components/Layout/SearchBar";
+import Logs from "./Components/logs/Logs";
+import AddBtn from "./Components/Layout/AddBtn";
+import AddLogModal from "./Components/logs/AddLogModal";
+import EditLogModal from "./Components/logs/EditLogModal";
+import AddTechModal from "./Components/Techs/AddTechModal";
+import TechListModal from "./Components/Techs/TechListModal";
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize CSS
+    M.AutoInit();
+  });
+
   return (
-    <div className="App">
-      <h2>React working</h2>
-    </div>
+    <React.Fragment>
+      <Searchbar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
