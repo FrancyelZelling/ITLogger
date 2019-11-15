8b2d03b9c761fa5db86455b36ae0032403acf1a2
Francyel Zelling <zellingf@gmail.com>
2019-11-15 10:24:14 -0300

    Redux For State Management section done

--- src/Components/logs/Logs.js
+++ src/Components/logs/Logs.js
@@ -1,28 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLogs } from "../../action/logActions";
 
 import LogItem from "./LogItem";
 import Preloader from "../Layout/Preloader";
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     // eslint-disable-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-
-    const res = await fetch("/logs");
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
@@ -40,4 +30,13 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.propTypes = {
+  logs: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  log: state.log,
+  getLogs: PropTypes.func.isRequired
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,14 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "../action/types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  SEARCH_LOGS
+} from "../action/types";
 
 const initialState = {
   logs: null,
@@ -12,10 +22,64 @@ export default (state = initialState, action) => {
     default:
       return state;
 
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false
+      };
+
+    case ADD_LOG:
+      return {
+        ...state,
+        logs: [...state.logs, action.payload],
+        loading: false
+      };
+
+    case DELETE_LOG:
+      return {
+        ...state,
+        logs: state.logs.filter(log => log.id !== action.payload),
+        loading: false
+      };
+
+    case UPDATE_LOG:
+      return {
+        ...state,
+        logs: state.logs.map(log =>
+          log.id === action.payload.id ? action.payload : log
+        )
+      };
+
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload
+      };
+
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload
+      };
+
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null
+      };
+
     case SET_LOADING:
       return {
         ...state,
         loading: true
+      };
+
+    case LOGS_ERROR:
+      console.log(action.payload);
+      return {
+        ...state,
+        error: action.payload
       };
   }
 };
